5cf4261c97a16af6ba17538fa57341cb1082f274
Ashok Hariharan <ashok@hariharan.org.in>
2013-11-27 17:51:33 +0300

    Updating project - adding unit test , updated maven script

---
+++ src/test/java/org/akomantoso/AnValidatorTest.java
@@ -0,0 +1,22 @@
+package org.akomantoso;
+
+/**
+ * org.akomantoso
+ * User: ashok
+ * Date: 11/26/13
+ * Time: 5:21 PM
+ */
+public class AnValidatorTest {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testValidate() throws Exception {
+
+    }
+}
