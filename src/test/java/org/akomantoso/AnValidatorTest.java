f5e075762a2be31dc79725156c46578dc2b06378
Ashok Hariharan <ashok@hariharan.org.in>
2013-11-27 17:54:20 +0300

    Adding skeleton unit tests

--- src/test/java/org/akomantoso/AnValidatorTest.java
+++ src/test/java/org/akomantoso/AnValidatorTest.java
@@ -1,12 +1,14 @@
 package org.akomantoso;
 
+import junit.framework.TestCase;
+
 /**
  * org.akomantoso
  * User: ashok
  * Date: 11/26/13
  * Time: 5:21 PM
  */
-public class AnValidatorTest {
+public class AnValidatorTest extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
 
