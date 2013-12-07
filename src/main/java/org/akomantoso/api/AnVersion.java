5d15eed3746c9f8b7ada1d5394f65ae419377611
Ashok Hariharan <ashok@hariharan.org.in>
2013-12-07 21:35:01 +0300

    Adding some documentary comments on the conventions for packaging schemas

--- src/main/java/org/akomantoso/api/AnVersion.java
+++ src/main/java/org/akomantoso/api/AnVersion.java
@@ -2,7 +2,6 @@ package org.akomantoso.api;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.HashMap;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import org.slf4j.Logger;
@@ -21,7 +20,11 @@ public final class AnVersion {
     Integer majorVersion;
     String minorVersion;
     
+    // the schema versions are always package within this package
+    // hierarchy
     public final String prefix = "org.akomantoso.schema.";
+    // the schema in the version package of the schema must always be called
+    // by this name
     public final String anxsd = "schema.xsd";
 
     /**
@@ -40,6 +43,8 @@ public final class AnVersion {
     /**
      * Use this Constructor if there is no minor version
      * @param majorVersion 
+     * @throws FileNotFoundException when a version that does not exist in the package
+     * is requested in the parameter
      */
     public AnVersion(Integer majorVersion) throws FileNotFoundException{
         this(majorVersion, null);
@@ -51,10 +56,14 @@ public final class AnVersion {
         String schemaPackagePath = schemaPackage.replace(".", "/");
         if (getClass().getClassLoader().getResource(schemaPackagePath) != null){
            if (getClass().getClassLoader().getResource(schemaPackagePath + "/schema.xsd") == null) {
-               throw new FileNotFoundException("The schema for the version does not exist !");
+               throw new FileNotFoundException(
+                       "The schema for the version does not exist !"
+                       );
            }
         } else {
-            throw new FileNotFoundException("The package for the version does not exist !");
+            throw new FileNotFoundException(
+                    "The package for the version does not exist !"
+                    );
         }
     }
     
@@ -63,7 +72,6 @@ public final class AnVersion {
      * @return 
      */
     public String getPackageForVersion(){
-        
         return this.prefix.
                 concat("v").concat(this.majorVersion.toString()).
                     concat(
@@ -71,15 +79,6 @@ public final class AnVersion {
                         "" :
                         "".concat(".").concat(this.minorVersion)
                     );
-        /**
-        return schemaInfo.get(majorVersion).prefix.concat("_").
-                concat(majorVersion.replace(".", "_")).
-                    concat(
-                        minorVersion == null ? 
-                        "" : 
-                        "".concat(".").concat(minorVersion)
-                );
-                */ 
     }
     
     /**
